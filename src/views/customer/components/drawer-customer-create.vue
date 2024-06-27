a02586dbe4e2ae59ce2bf9a2167644051c838595
Vica <731661902@qq.com>
2024-06-27 10:02:52 +0800

    filter file

--- src/views/customer/components/drawer-customer-create.vue
+++ src/views/customer/components/drawer-customer-create.vue
@@ -3,7 +3,7 @@ import {defineProps, inject, reactive, ref, watch} from "vue";
 import HighlightWords from 'vue-highlight-words';
 import {useI18n} from "vue-i18n";
 import dynamicIcon from "@/components/custom/DynamicIcon.vue";
-
+import {Message} from '@arco-design/web-vue';
 
 const modalsState = inject('modalsState');
 const setNewCustomer = inject('setNewCustomer');
@@ -30,55 +30,66 @@ const props = defineProps({
       list: [{
         label: 'customer.givenName',
         value: '--',
+        key: 'real_name',
         span: 2,
         type: 'text'
       }, {
         label: 'customer.lastName',
         value: '--',
+        key: 'last_name',
         span: 2,
         type: 'text'
       }, {
         label: 'customer.email',
         value: '--',
+        key: 'email',
         span: 2,
         type: 'text'
       }, {
         label: 'customer.phoneNumber',
         value: '--',
+        key: 'phone',
         span: 2,
         type: 'phone'
       }, {
         label: 'customer.birthday',
         value: '--',
+        key: 'birthday',
         span: 2,
         type: 'date'
       }, {
         label: 'customer.gender',
         value: '--',
+        key: 'sex',
         span: 2,
         type: 'radio',
       }, {
         label: 'customer.country',
+        key: 'country',
         value: '--',
         span: 2,
         type: 'select',
       }, {
         label: 'customer.city',
         value: '--',
+        key: 'city',
         span: 2,
         type: 'select',
       }, {
         label: 'customer.district',
         value: '--',
+        key: 'area',
         span: 3,
         type: 'select',
       }, {
         label: 'customer.postalCode',
+        key: 'postal_code',
         value: '--',
         span: 3,
         type: 'text',
       }, {
         label: 'customer.address',
+        key: 'address',
         value: '--',
         span: 3,
         type: 'text',
@@ -87,20 +98,69 @@ const props = defineProps({
   }
 });
 
-const customerInfo = ref({
-  customerCountCode: 1
+const customerInfo = reactive({
+  countCode: 1,
+  country: '',
+  city: null,
+  area: null,
+  province: null,
+  address: null,
+  email: null,
+  phone: null,
+  real_name: null,
+  last_name: null,
+  sex: '0',
+  birthday: null,
+  postal_code: null
+});
+
+const customerInfoFrom = reactive({
+  countCode: true,
+  country: true,
+  city: true,
+  area: true,
+  province: true,
+  address: true,
+  email: true,
+  phone: true,
+  real_name: true,
+  last_name: true,
+  sex: true,
+  birthday: true,
+  postal_code: true,
+  agree: true
 });
 
 const loading = ref(false);
 
 const options = [
-  {label: 'customer.female', value: '1'},
-  {label: 'customer.male', value: '2'},
-  {label: 'customer.sexSecret', value: '3'},
+  {label: 'customer.female', value: '0'},
+  {label: 'customer.male', value: '1'},
+  {label: 'customer.sexSecret', value: '2'},
 ];
 
+
+const handleBlur = (key) => {
+
+  customerInfoFrom[key] = customerInfo[key];
+
+}
+
 const createCustomer = () => {
-  loading.value = true;
+  let isValid = true;
+  for (const k in customerInfoFrom) {
+    handleBlur(k)
+    if (!customerInfoFrom[k]) {
+      isValid = false;
+    }
+  }
+  if (!isValid) {
+    // 如果验证不通过，可以在这里进行相应处理，例如设置错误状态等
+    Message.error("Please check if the customer information is complete.")
+  } else {
+    //提交到后台
+
+  }
 }
 
 </script>
@@ -115,21 +175,26 @@ const createCustomer = () => {
           <a-descriptions v-for="row in customerInfoForm" style="margin-bottom: 25px;" :title="t(row.itemName)"
                           :column="{ xs: 2, sm: 4, md: 4, lg: 4 }" layout="inline-vertical" bordered>
             <a-descriptions-item v-for="item of row.list" :label="t(item.label)" :span="item.span">
-              <span class="customer-create-item flex items-center">
-                <a-select v-if="['phone'].includes(item.type)" default-value="+86" :style="{width:'80px'}" size="small"
-                          class="bg-white mr-1 hover:bg-white">
-                 <a-option :value="1">+86</a-option>
-                </a-select>
-                <a-input v-if="['phone','text'].includes(item.type)"
-                         :placeholder="t('placeholder.prefix')+t(item.label)" class="bg-white hover:bg-white border-0"/>
-                <a-radio-group v-if="['radio'].includes(item.type)" :options="options">
+              <span class="customer-create-item flex items-center relative">
+                <a-input-group v-if="['phone'].includes(item.type)">
+                  <a-select :options="['+86','+1','+65']" size="small" :style="{width:'80px'}" class="border-0" style="border-right: none;padding-left: 10px;" />
+                  <a-input class="bg-white hover:bg-white border-0" style="padding-left: 5px;" :placeholder="t('placeholder.prefix')+t(item.label)" v-model="customerInfo[item.key]" @blur="handleBlur(item.key)" />
+                </a-input-group>
+                <a-input v-if="['text'].includes(item.type)"
+                         :placeholder="t('placeholder.prefix')+t(item.label)" v-model="customerInfo[item.key]"
+                         class="bg-white hover:bg-white border-0" @blur="handleBlur(item.key)">
+                </a-input>
+                <a-radio-group v-if="['radio'].includes(item.type)" v-model="customerInfo[item.key]" :options="options">
                     <template #label="{ data }">
                       <span>{{ t(data.label) }}</span>
                     </template>
                 </a-radio-group>
-                <a-select v-if="['select'].includes(item.type)" size="small"
-                          :placeholder="t('placeholder.prefix')+t(item.label)" class="bg-white  mr-1 hover:bg-white"/>
-                <a-date-picker v-if="['date'].includes(item.type)" class="bg-white hover:bg-white border-0"/>
+                <a-select v-if="['select'].includes(item.type)" size="small" v-model="customerInfo[item.key]"
+                          :placeholder="t('placeholder.prefix')+t(item.label)"  @change="handleBlur(item.key)" @popup-visible-change="handleBlur(item.key)" class="bg-white  mr-1 hover:bg-white"/>
+                <a-date-picker v-if="['date'].includes(item.type)" v-model="customerInfo[item.key]" @change="handleBlur(item.key)" @popup-visible-change="handleBlur(item.key)"
+                               class="bg-white hover:bg-white border-0">
+                </a-date-picker>
+                <icon-close-circle class="text-red-600 absolute right-0" v-if="!customerInfoFrom[item.key]"/>
               </span>
             </a-descriptions-item>
           </a-descriptions>
@@ -137,10 +202,13 @@ const createCustomer = () => {
           <div class="p-5 w-2/3 m-auto">
             <div class="mb-5 text-center">
               <a-checkbox value="1">
-                <a href="" class="border-b border-gray-500">{{ t('customer.policy') }}</a>
+                <a href="" :class="['border-b border-gray-500',{'text-red-500':customerInfoFrom.agree===false}]">{{
+                    t('customer.policy')
+                  }}</a>
               </a-checkbox>
             </div>
-            <a-button type="primary" class="h-12 font-bold text-lg" shape="round" @click="createCustomer" long>{{
+            <a-button type="primary" class="h-12 font-bold text-lg" shape="round" @click="createCustomer" long>
+              {{
                 t('modal.footer.create')
               }}
             </a-button>
